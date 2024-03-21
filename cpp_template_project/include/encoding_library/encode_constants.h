8fb646e72c7877698b08a5e4d2073a107df5185e
Dasha Mokievets <dasha.mok02@gmail.com>
2024-03-21 14:50:38 +0100

    constants

---
+++ cpp_template_project/include/encoding_library/encode_constants.h
@@ -0,0 +1,19 @@
+#pragma once
+
+namespace en_const {
+constexpr int eighth_bit = 128;
+constexpr int seventh_bit = 64;
+constexpr int sixth_bit = 32;
+constexpr int fifth_bit = 16;
+constexpr int fourth_bit = 8;
+constexpr int third_bit = 4;
+constexpr int second_bit = 2;
+constexpr int first_bit = 1;
+constexpr unsigned char bool_true_tag = 32;
+constexpr unsigned char bool_false_tag = 64;
+constexpr unsigned char start_end_tag = 0;
+constexpr int bits = 8;
+constexpr int bytes_for_size = 4;
+constexpr unsigned char undefined_type_tag = 20;
+constexpr int first_six_bytes = 6;
+}  // namespace en_const
